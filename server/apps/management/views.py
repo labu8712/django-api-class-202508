6886a2fb3bddc0939b9e078c95281193730aeef1
Arthur Chang <arthurc0102@gmail.com>
2025-08-16 14:57:02 +0800

    Day02 - 008

--- server/apps/management/views.py
+++ server/apps/management/views.py
@@ -4,4 +4,9 @@ from rest_framework.response import Response
 
 @api_view(["GET", "POST"])
 def hello(request):
-    return Response({"message": "Hello World!"})
+    if request.method == "GET":
+        message = "Hello World by GET method"
+    else:
+        message = "Hello World by POST method"
+
+    return Response({"message": message})
