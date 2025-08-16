a391e89bcefc85f6bee75b5c9b37d0b2d83e0d39
Arthur Chang <arthurc0102@gmail.com>
2025-08-16 13:20:59 +0800

    Day02 - 003

--- server/apps/management/admin.py
+++ server/apps/management/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from server.apps.management.models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
