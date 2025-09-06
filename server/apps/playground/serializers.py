c75404a9d13950a8415c761cb2827b1e83448a35
Arthur Chang <arthurc0102@gmail.com>
2025-09-06 14:45:44 +0800

    Day04 - 015

--- server/apps/playground/serializers.py
+++ server/apps/playground/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from server.apps.playground.models import Item
+from server.apps.playground.models import Item, ItemComment
+
+
+class ItemCommentInItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemComment
+        fields = ("id", "content", "created_at", "updated_at")
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    comments = ItemCommentInItemSerializer(read_only=True, many=True)
+
     class Meta:
         model = Item
         fields = (
@@ -11,4 +19,5 @@ class ItemSerializer(serializers.ModelSerializer):
             "name",
             "description",
             "is_active",
+            "comments",
         )
